0982f1b1b186f01a2a0d13bf71d642aab456ce36
Isaque Victor Duarte De Oliveira <45144815+109isaque10@users.noreply.github.com>
2024-07-11 13:51:33 -0300

    Update torrentioelf.py

--- providers/a4kScrapers/en/torrent/torrentioelf.py
+++ providers/a4kScrapers/en/torrent/torrentioelf.py
@@ -31,15 +31,9 @@ class sources(core.DefaultSources):
     def _search_request(self, url, query):
         query = self._imdb
         if not self.is_movie_query():
-            query += ':' + self.scraper.season_x + ':' + self.scraper.episode_x
+            query += '&season=' + self.scraper.season_x
 
-        params = 'language=english|debridoptions=nodownloadlinks,nocatalog'
-        if self._pm_apikey:
-            params += '|premiumize=' + self._pm_apikey
-        if self._rd_apikey:
-            params += '|realdebrid=' + self._rd_apikey
-        if self._ad_apikey:
-            params += '|alldebrid=' + self._ad_apikey
+        params = '&apikey=36uoqvh24k5gc0ljvk8y7hagngnkeql4' 
 
         request_url = url.base + '/' + core.quote_plus(params) + (url.search % core.quote_plus(query))
         response = self._request.get(request_url)
@@ -48,41 +42,25 @@ class sources(core.DefaultSources):
             return []
 
         try:
-            results = core.json.loads(response.text)
+            results = bs(response.text, 'html5lib')
+            rows = results('item')
         except Exception as e:
             self._request.exc_msg = 'Failed to parse json: %s' % response.text
             return []
 
-        if not results or 'streams' not in results or len(results['streams']) == 0:
+        if not rows:
             return []
         else:
-            return results['streams']
+            return rows
 
     def _soup_filter(self, response):
         return response
 
-    def _title_filter(self, el):
-        if not self.is_movie_query():
-            parts = el['title'].split('\n')
-            #if len(parts) > 1 and '👤' not in parts[1]:
-            if len(parts) > 1 and '💾' not in parts[1]:
-                parts.pop(1)
-                return '\n'.join(parts)
-
-        return el['title']
-
     def _info(self, el, url, torrent):
         if 'infoHash' in el:
             torrent['hash'] = el['infoHash']
-        if 'url' in el:
+        if 'link' in el:
             torrent['url'] = el['url']
-
-        if '[PM+]' in el['name']:
-            torrent['debrid'] = 'PM'
-        if '[RD+]' in el['name']:
-            torrent['debrid'] = 'RD'
-        if '[AD+]' in el['name']:
-            torrent['debrid'] = 'AD'
 
         torrent['size'] = core.source_utils.de_string_size(self.genericScraper.parse_size(el['title']))
         torrent['seeds'] = self.genericScraper.parse_seeds(el['title'])
@@ -94,19 +72,11 @@ class sources(core.DefaultSources):
 
         return torrent
 
-    def _set_apikeys(self, kwargs):
-        apikeys = kwargs.get('apikeys', {})
-        self._pm_apikey = apikeys.get('pm', None)
-        self._rd_apikey = apikeys.get('rd', None)
-        self._ad_apikey = apikeys.get('ad', None)
-
     def movie(self, title, year, imdb=None, **kwargs):
-        self._set_apikeys(kwargs)
         self._imdb = imdb
         return super(sources, self).movie(title, year, imdb, auto_query=False)
 
     def episode(self, simple_info, all_info, **kwargs):
-        self._set_apikeys(kwargs)
         self._imdb = all_info.get('info', {}).get('tvshow.imdb_id', None)
         if self._imdb is None:
             self._imdb = all_info.get('showInfo', {}).get('ids', {}).get('imdb', None)
